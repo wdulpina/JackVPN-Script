4d6a2cc6f22042dad566b43f95a2599f34a509f7
Status404Error <35353099+Status404Error@users.noreply.github.com>
2019-03-03 14:29:33 +0800

    Update server-message.sh

--- server-message.sh
+++ server-message.sh
@@ -9,23 +9,8 @@ fi
  
 cat > /etc/issue.net <<-END
 <br><font color='#000000'>=======================================</br></font>
-<br><font color='#008080'>***************** <b>Team StefanWorks SERVER</b> ****************</br></font>
-<br><font color='#000000'>=======================================</br></font>
-<br></br>
-<br><font color='#FF0000'>****************** <b>!!!WARNING!!!</b> ******************</br></font>
-<br></br>
-<br><font color='#860000'>MAGNANAKAW NG SERVER</br></font>
-<br><font color='#1E90FF'>ANG NAG POST NG EHI NA 'TO</br></font>
-<br><font color='#FF0000'>WALANG RESPETO</br></font>
-<br><font color='#008080'>DI MA LANG NAGPAALAM</br></font>
-<br><font color='#BA55D3'>KUNG SINO KA MAN</br></font>
-<br><font color='#32CD32'>TIGILAN MO NA YAN</br></font>
-<br></br>
-<br><font color='#FF0000'>FOLLOW THE RULES OR</br></font>
-<br><font color='#FF0000'>ANG FB ACCOUNT MO MASUSUNOG</br></font>
-<br></br>
-<br><font color='#000000'>=======================================</br></font>
-<br><font color='#0000FF'>************** <b>EXCLUSIVE FOR TSWVPN SERVER ONLY</b> ***************</br></font>
+<br><font color='#0000FF'>************** <b>EXCLUSIVE FOR JackVPN SERVER ONLY</b> ***************</br></font>
+<br><font color='#FF0000'>************** <b>DO NOT USE FOR ANY MEANS OF USAGE</b> ****************</br></font>
 <br><font color='#000000'>=======================================</br></font>
 END
 
