d6be95682ba6231a4bbef6860c05ef9bd3043cd6
Status404Error <35353099+Status404Error@users.noreply.github.com>
2019-05-25 23:16:30 +0800

    Update jackvpn-ocs.sh

--- jackvpn-ocs.sh
+++ jackvpn-ocs.sh
@@ -40,11 +40,11 @@ echo "I need to ask some questions before starting setup"
 echo "You can leave the default option and just hit enter if you agree with the option"
 echo ""
 echo "First I need to know the new password of MySQL root user:"
-read -p "Password baru: " -e -i password DatabasePass
+read -p "Password baru: " -e -i jackvpnpanel DatabasePass
 echo ""
 echo "Finally, name the Database Name for OCS Panels"
 echo " Please, use one word only, no special characters other than Underscore (_)"
-read -p " Database Name: " -e -i OCS_PANEL DatabaseName
+read -p " Database Name: " -e -i JACK_PANEL DatabaseName
 echo ""
 echo "Okay, that's all I need. We are ready to setup your OCS Panels now"
 read -n1 -r -p "Press any key to continue..."
